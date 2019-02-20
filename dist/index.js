62694e697cadca6a9df15daa2c868e8c6409c877
Zac Rosenbauer <zrosenbauer@precognitive.io>
2019-02-20 09:32:46 -0600

    remove dist files

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function autoRule(user, context, callback) {
-    return true;
-}
-exports.autoRule = autoRule;
-//# sourceMappingURL=index.js.map
